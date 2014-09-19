966d764f4ed97a835ea50ef20031000544f98ead
jpnexos <jpgonzalez@nexos-software.com.co>
2014-09-19 16:22:24 -0500

    update config
    update config

--- core/core.js
+++ core/core.js
@@ -35,7 +35,7 @@ if(datosUsuario=="" || datosPassword==""){
    alert("Los campos no pueden estar vacios");
    return false;
 }
-var url=siteCustomer + '/Consumidor/Filter/?usuario1='+datosUsuario+'&clave='+datosPassword+'';
+var url=siteCustomer + '/Consumidor/Filter/?usuario='+datosUsuario+'&clave='+datosPassword+'';
 $.ajax({ // ajax call starts
     url: url, // JQuery loads serverside.php 
     type: "GET",
